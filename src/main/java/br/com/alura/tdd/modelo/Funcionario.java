47a5d704326c1b60045d92de5694b6b05d7e79ca
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-03 19:12:43 -0300

    Como testar métodos privados?

--- src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -29,6 +29,12 @@ public class Funcionario {
 	}
 
     public void reajustarSalario(BigDecimal reajuste) {
-		this.salario = this.salario.add(reajuste).setScale(2, RoundingMode.HALF_UP);
+		this.salario = this.salario.add(reajuste);
+		arredondarSalario();
     }
+
+	// MÉTODO PRIVADOS NÃO SÃO TESTADOS!
+	private void arredondarSalario() {
+		this.salario = this.salario.setScale(2, RoundingMode.HALF_UP);
+	}
 }
